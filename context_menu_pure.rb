85af4187801144209f32a4ec3db02aedd0964295
KINGSABRI <king.sabri@gmail.com>
2017-02-04 19:35:29 +0300

    Add context_menu_pure.rb fix indentation

--- context_menu_pure.rb
+++ context_menu_pure.rb
@@ -32,7 +32,7 @@ class BurpExtender
     @callbacks.setExtensionName('Right-click Menu')     # Set Extension name
     @callbacks.registerContextMenuFactory(self)         # register a factory for custom context menu items.
   end
-
+  
   # createMenuItems(IContextMenuInvocation invocation)
   # This method will be called by Burp when the user invokes a context menu anywhere within Burp.
   #
@@ -40,27 +40,26 @@ class BurpExtender
   # An object that implements the IMessageEditorTabFactory interface,
   #
   # @return Array
- def createMenuItems(invocation)
-   
-   menu_list = []   # Array of menus datastore
-   
-   menuItem = JMenuItem.new('<html><b>Ruby | Menu</b></html>')          # Create a regular menuitem
-   menuItem.addActionListener { puts 'An action has been performed' }   # addActionLinstener takes a block of actions
-
-   # Create Section menu (contains sub-menus)
-   sectionsMenu = JMenu.new('<html><b>Ruby | Menu with sub-menus (Sections Menu)</b></html>')
-   menuItem1 = JMenuItem.new('Sub-menuItem 1')                          # Create a regular menuitem
-   menuItem1.addActionListener { puts 'Yup, menuItem 1 clicked!' }      # Add an action
-   sectionsMenu.add(menuItem1)                                          # Add it to it's parent, the section menu
-   menuItem2 = JMenuItem.new('Sub-menuItem 2')
-   menuItem2.addActionListener { puts 'Yup, menuItem 2 clicked!' }
-   sectionsMenu.add(menuItem2)
-   
-   # All menus has to be added to the array
-   menu_list << menuItem
-   menu_list << sectionsMenu
+  def createMenuItems(invocation)
+    menu_list = []   # Array of menus datastore
+    
+    menuItem = JMenuItem.new('<html><b>Ruby | Menu</b></html>')          # Create a regular menuitem
+    menuItem.addActionListener { puts 'An action has been performed' }   # addActionLinstener takes a block of actions
+    
+    # Create Section menu (contains sub-menus)
+    sectionsMenu = JMenu.new('<html><b>Ruby | Menu with sub-menus (Sections Menu)</b></html>')
+    menuItem1 = JMenuItem.new('Sub-menuItem 1')                          # Create a regular menuitem
+    menuItem1.addActionListener { puts 'Yup, menuItem 1 clicked!' }      # Add an action
+    sectionsMenu.add(menuItem1)                                          # Add it to it's parent, the section menu
+    menuItem2 = JMenuItem.new('Sub-menuItem 2')
+    menuItem2.addActionListener { puts 'Yup, menuItem 2 clicked!' }
+    sectionsMenu.add(menuItem2)
+    
+    # All menus has to be added to the array
+    menu_list << menuItem
+    menu_list << sectionsMenu
   end
-  
+
 end
 
 
